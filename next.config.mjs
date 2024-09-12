e8cf56021754a6d60c33aa665c429acb6e106b1c
sobit-woxsen <sobit.prasad@woxsen.edu.in>
2024-09-12 16:40:28 +0530

    config file

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  transpilePackages: ["@aws-sdk", "@smithy"],
+};
