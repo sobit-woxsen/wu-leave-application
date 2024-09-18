ed5b1c794995ac420d53a1512a5ffb3e133e14cf
sobit-woxsen <sobit.prasad@woxsen.edu.in>
2024-09-18 12:15:37 +0530

    test

--- src/app/api/student/logout/route.ts
+++ src/app/api/student/logout/route.ts
@@ -6,8 +6,8 @@ export async function POST() {
 
   cookieStore.set("token", "", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: false,
+    sameSite: "lax",
     maxAge: 0,
     path: "/",
   });
